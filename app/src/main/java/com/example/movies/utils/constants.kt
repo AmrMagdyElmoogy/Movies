ca27f9786fb0e35d358d19eee4a9f1f1d0ff9c85
AmrMagdyElmoogy <amrelmoogy063@gmail.com>
2024-03-04 19:24:58 +0200

    Setup Commit

---
+++ app/src/main/java/com/example/movies/utils/constants.kt
@@ -0,0 +1,6 @@
+package com.example.movies.utils
+
+// Remember
+const val API_KEY = "d796e5f1005d4b154a4eb644fc9b0f38"
+const val BASE_URL = "https://api.themoviedb.org/3/movie"
+const val NOW_PLAYING = "/now_playing"
